5f8377b43c4bf75f417742a3824a1d0688b7c4e0
Valentin MOREAU <vmoreau@datasolution.fr>
2022-10-07 11:54:40 +0200

    add login and subscribe pages

---
+++ src/Repository/IdTrickRepository.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Repository;
+
+use App\Entity\IdTrick;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\Persistence\ManagerRegistry;
+
+/**
+ * @extends ServiceEntityRepository<IdTrick>
+ *
+ * @method IdTrick|null find($id, $lockMode = null, $lockVersion = null)
+ * @method IdTrick|null findOneBy(array $criteria, array $orderBy = null)
+ * @method IdTrick[]    findAll()
+ * @method IdTrick[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
+ */
+class IdTrickRepository extends ServiceEntityRepository
+{
+    public function __construct(ManagerRegistry $registry)
+    {
+        parent::__construct($registry, IdTrick::class);
+    }
+
+    public function add(IdTrick $entity, bool $flush = false): void
+    {
+        $this->getEntityManager()->persist($entity);
+
+        if ($flush) {
+            $this->getEntityManager()->flush();
+        }
+    }
+
+    public function remove(IdTrick $entity, bool $flush = false): void
+    {
+        $this->getEntityManager()->remove($entity);
+
+        if ($flush) {
+            $this->getEntityManager()->flush();
+        }
+    }
+
+//    /**
+//     * @return IdTrick[] Returns an array of IdTrick objects
+//     */
+//    public function findByExampleField($value): array
+//    {
+//        return $this->createQueryBuilder('i')
+//            ->andWhere('i.exampleField = :val')
+//            ->setParameter('val', $value)
+//            ->orderBy('i.id', 'ASC')
+//            ->setMaxResults(10)
+//            ->getQuery()
+//            ->getResult()
+//        ;
+//    }
+
+//    public function findOneBySomeField($value): ?IdTrick
+//    {
+//        return $this->createQueryBuilder('i')
+//            ->andWhere('i.exampleField = :val')
+//            ->setParameter('val', $value)
+//            ->getQuery()
+//            ->getOneOrNullResult()
+//        ;
+//    }
+}
